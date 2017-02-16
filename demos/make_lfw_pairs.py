55c1fb915442c9af7d8c30dd51af430ebdf4270f
Florent Buisson <florent.buisson.1987@gmail.com>
2017-02-16 20:07:35 +0100

    Make different pairs

---
+++ demos/make_lfw_pairs.py
@@ -0,0 +1,80 @@
+"""Generates pairs file for the LFW dataset
+"""
+# MIT License
+#
+# Copyright (c) 2017 Florent Buisson
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import sys
+import argparse
+import itertools
+import random
+
+
+def listdir_nohidden_gen(path):
+    for f in os.listdir(path):
+        if os.path.isdir(os.path.join(path, f)) and not f.startswith('.'):
+            yield os.path.join(path, f)
+
+
+def pop_random(lst):
+    idx = random.randrange(0, len(lst))
+    return idx, lst.pop(idx)
+
+
+def main(args):
+    i = 0
+    dirs = list(listdir_nohidden_gen(args.lfw_dir))
+
+    print(len(dirs))
+
+    with open('/tmp/diff_pairs.txt', 'w') as pairs_file:
+        while len(dirs) > 0:
+            dirs_copy = dirs.copy()
+            _, dir1 = pop_random(dirs)
+            files1 = os.listdir(dir1)
+            while len(files1) > 0 and len(dirs_copy) > 0:
+                file1, _ = pop_random(files1)
+                _, dir2 = pop_random(dirs_copy)
+                files2 = os.listdir(dir2)
+                file2, _ = pop_random(files2)
+                pairs_file.write(
+                    os.path.basename(dir1) + " " + str(file1 + 1) + " " + os.path.basename(dir2) + " " + str(
+                        file2 + 1) + "\n")
+                i += 1
+                if i % 100 == 0:
+                    print(i)
+
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--lfw_dir',
+        type=str,
+        default='/Users/bushi/datasets/face_recognition/lfw/lfw-mtcnnpy_160',
+        help='Path to LFW dataset.'
+    )
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
